9961e1f17dcb17346f82e862dd8f262eb0ff4f31
cryne <crisonic123@hotmail.com>
2020-08-30 17:32:20 -0600

    Crea archivos

--- src/calichescript/main.java
+++ src/calichescript/main.java
@@ -9,7 +9,12 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFileChooser;
@@ -38,6 +43,14 @@ public class main extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        New_file_JF = new javax.swing.JFrame();
+        jPanel2 = new javax.swing.JPanel();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        txt_Ncode = new javax.swing.JTextArea();
+        Ok_BT = new javax.swing.JButton();
+        Cancel_BT = new javax.swing.JButton();
+        name_tf = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         txt_code = new javax.swing.JTextArea();
@@ -47,11 +60,94 @@ public class main extends javax.swing.JFrame {
         jButton2 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
+        Save_BT = new javax.swing.JButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         menu_openFile = new javax.swing.JMenuItem();
         menu_genFlex = new javax.swing.JMenuItem();
+        jMenuItem1 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
+
+        New_file_JF.setBackground(new java.awt.Color(0, 0, 0));
+        New_file_JF.addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosed(java.awt.event.WindowEvent evt) {
+                New_file_JFWindowClosed(evt);
+            }
+        });
+
+        jPanel2.setBackground(new java.awt.Color(0, 0, 0));
+
+        txt_Ncode.setColumns(20);
+        txt_Ncode.setRows(5);
+        jScrollPane3.setViewportView(txt_Ncode);
+
+        Ok_BT.setText("Ok");
+        Ok_BT.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Ok_BTActionPerformed(evt);
+            }
+        });
+
+        Cancel_BT.setText("Cancel");
+        Cancel_BT.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Cancel_BTActionPerformed(evt);
+            }
+        });
+
+        name_tf.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                name_tfActionPerformed(evt);
+            }
+        });
+
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setText("Nombre");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(29, 29, 29)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(name_tf, javax.swing.GroupLayout.PREFERRED_SIZE, 157, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(Cancel_BT)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(Ok_BT)
+                        .addGap(30, 30, 30))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 578, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(27, Short.MAX_VALUE))))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(27, 27, 27)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 419, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(Ok_BT)
+                    .addComponent(Cancel_BT)
+                    .addComponent(name_tf, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3))
+                .addContainerGap(17, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout New_file_JFLayout = new javax.swing.GroupLayout(New_file_JF.getContentPane());
+        New_file_JF.getContentPane().setLayout(New_file_JFLayout);
+        New_file_JFLayout.setHorizontalGroup(
+            New_file_JFLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        New_file_JFLayout.setVerticalGroup(
+            New_file_JFLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -80,6 +176,14 @@ public class main extends javax.swing.JFrame {
         jLabel2.setForeground(new java.awt.Color(255, 255, 255));
         jLabel2.setText("Salida");
 
+        Save_BT.setText("Guardar");
+        Save_BT.setEnabled(false);
+        Save_BT.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Save_BTActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -95,7 +199,9 @@ public class main extends javax.swing.JFrame {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                             .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(Save_BT))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addComponent(btn_run)
                                 .addGap(18, 18, 18)
@@ -117,6 +223,8 @@ public class main extends javax.swing.JFrame {
                             .addComponent(jButton2, javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(btn_run)))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(Save_BT)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jLabel2)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
@@ -148,6 +256,15 @@ public class main extends javax.swing.JFrame {
             }
         });
         jMenu1.add(menu_genFlex);
+
+        jMenuItem1.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_N, java.awt.event.InputEvent.CTRL_MASK));
+        jMenuItem1.setText("Nuevo Archivo");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem1);
 
         jMenuBar1.add(jMenu1);
 
@@ -181,6 +298,7 @@ public class main extends javax.swing.JFrame {
                 out += st + "\n";
             }
             br.close();
+            this.Save_BT.setEnabled(true);
         } else {
             JOptionPane.showMessageDialog(this, "No se cargo ningun archivo");
         }
@@ -249,6 +367,64 @@ public class main extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_btn_runActionPerformed
 
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        //this.setVisible(false);
+        this.New_file_JF.pack();
+        this.New_file_JF.setVisible(true);
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
+
+    private void Ok_BTActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Ok_BTActionPerformed
+        if (!name_tf.getText().equals("")) {
+            Path path = Paths.get("./test_files/"+this.name_tf.getText()+".cal");
+            try {
+                Files.createDirectories(path.getParent());
+                Files.createFile(path);
+                PrintWriter writer = new PrintWriter(new FileWriter(path.toString(),true));
+                writer.append(this.txt_Ncode.getText());
+                writer.close();
+                System.out.println("se creo");
+                this.New_file_JF.setVisible(false);
+                this.input_file=new File("./test_files/"+this.name_tf.getText()+".cal");
+                this.txt_Ncode.setText("");
+                this.name_tf.setText("");
+                this.txt_code.setText(this.getText(input_file));
+                this.Save_BT.setEnabled(true);
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+        }else{
+            System.out.println("Elija un nombre valido para el archivo");
+        }
+    }//GEN-LAST:event_Ok_BTActionPerformed
+
+    private void New_file_JFWindowClosed(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_New_file_JFWindowClosed
+
+    }//GEN-LAST:event_New_file_JFWindowClosed
+
+    private void Save_BTActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Save_BTActionPerformed
+        try {
+            String temp=this.txt_code.getText();
+            System.out.println("-----"+this.txt_code.getText().equals(this.getText(input_file)));
+            this.txt_code.setText(temp);
+        } catch (Exception ex) {
+            
+        }
+        
+    }//GEN-LAST:event_Save_BTActionPerformed
+
+    private void Cancel_BTActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Cancel_BTActionPerformed
+        this.New_file_JF.setVisible(false);
+        this.txt_Ncode.setText("");
+        this.name_tf.setText("");
+    }//GEN-LAST:event_Cancel_BTActionPerformed
+
+    private void name_tfActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_name_tfActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_name_tfActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -286,20 +462,31 @@ public class main extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Cancel_BT;
+    private javax.swing.JFrame New_file_JF;
+    private javax.swing.JButton Ok_BT;
+    private javax.swing.JButton Save_BT;
     private javax.swing.JButton btn_run;
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JMenuItem menu_genFlex;
     private javax.swing.JMenuItem menu_openFile;
+    private javax.swing.JTextField name_tf;
+    private javax.swing.JTextArea txt_Ncode;
     private javax.swing.JTextArea txt_code;
     private javax.swing.JTextArea txt_result;
     // End of variables declaration//GEN-END:variables
     private File input_file;
+    
 }
