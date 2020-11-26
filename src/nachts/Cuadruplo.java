fa9ff25543129f444afdf3b8e81b218526ff0277
CascoEresTu <k_k_ryuten@live.com>
2020-11-26 17:14:51 -0600

    y mis cambios?

--- src/nachts/Cuadruplo.java
+++ src/nachts/Cuadruplo.java
@@ -10,5 +10,59 @@ package nachts;
  * @author carlos
  */
 public class Cuadruplo {
-    
+
+    public String operator;
+    public String args1;
+    public String args2;
+    public String result;
+
+    public Cuadruplo(String operator, String args1, String args2, String result) {
+        this.operator = operator;
+        this.args1 = args1;
+        this.args2 = args2;
+        this.result = result;
+    }
+
+    public String getArgs1() {
+        return args1;
+    }
+
+    public void setArgs1(String args1) {
+        this.args1 = args1;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+    public String getArgs2() {
+        return args2;
+    }
+
+    public void setArgs2(String args2) {
+        this.args2 = args2;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String[] getTupla() {
+        String[] retval = {this.operator,this.args1,this.args2,this.result};
+        return retval;
+    }
+
+    @Override
+    public String toString() {
+        return "Cuadruplo{" + "operator=" + operator + ", args1=" + args1 + ", args2=" + args2 + ", result=" + result + '}';
+    }
+
 }
